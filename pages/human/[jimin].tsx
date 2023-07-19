2f01797f26bfa81768baa5358a795863d980c2e2
jiminKang <wlals1234560@gmail.com>
2023-07-19 17:52:08 +0900

    동적 페이지 이동

---
+++ pages/human/[jimin].tsx
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+const jimin = () => {
+    return(
+    <div>
+        <h2>hello!</h2>
+        <Link href="/">
+            <a>zz</a>
+        </Link>
+    </div>
+    )
+}
+
+export default jimin;
---
+++ pages/kang.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link"
+
+const Kang = () => {
+    return(
+        <div>
+            <h2>
+            <Link href="/">
+                <a>Move to /</a>
+            </Link>
+            </h2>
+        </div>
+    );
+};
+
+export default Kang;
